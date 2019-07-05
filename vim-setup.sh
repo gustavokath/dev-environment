01d5c7641abbcb2a1b11e192a54007b6c0c456b9
Gustavo Kath <gustavo.kath@acad.pucrs.br>
2019-07-05 09:57:56 -0300

    Add copy vimrc

--- vim-setup.sh
+++ vim-setup.sh
@@ -1,3 +1,6 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+curl -fLo ~/.vimrc --create-dirs https://raw.githubusercontent.com/gustavokath/gustavokath/master/.vimrc
+
+
 brew install fzf
